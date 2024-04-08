04f2e26d35515d9e9aa9784cdf4906e727d9d7bb
zykscode <zyskson@gmail.com>
2024-04-08 14:44:19 +0100

    first comit

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,4 +1,4 @@
-const Logo = ({ isOpen }) => {
+const Logo = ({ isOpen }: { isOpen: boolean }) => {
   return (
     <span
       className={`font-atyp z-50 self-center text-3xl font-semibold transition-colors duration-1000 ease-in-out md:p-4 ${isOpen ? 'text-light' : 'text-dark'}`}
